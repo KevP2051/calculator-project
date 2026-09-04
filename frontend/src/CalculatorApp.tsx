d49ba9c4aa3f7bde3e31ed434ce4370cd2d4f4ea
Kevin Poveda <kevinpovedaj@gmail.com>
2026-09-03 20:02:42 -0500

    feat: integrate @tanstack/react-query for data fetching in CalculatorApp

--- frontend/src/CalculatorApp.tsx
+++ frontend/src/CalculatorApp.tsx
@@ -1,9 +1,16 @@
 import CalculatorPage from "./calculator/pages/CalculatorPage"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 
 const CalculatorApp = () => {
     return (
         <>
-            <CalculatorPage />
+            <QueryClientProvider client={queryClient}>
+                <CalculatorPage />
+            </QueryClientProvider>
+
         </>
     )
 }
